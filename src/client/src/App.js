314fceccda374c2ee14204c73c1ab05eacb6681a
Rubén Ortiz <runerd.menethil@gmail.com>
2021-06-13 02:33:22 -0400

    feat(data) supply and demand data

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -27,24 +27,48 @@ function App() {
             backgroundColor: "rgb(147, 22, 33)",
             borderColor: "rgb(147, 22, 33)",
             data: randomPrices(0.03, 0.05),
+            get demand() {
+              return (
+                this.data[this.data.length - 1] -
+                this.data[this.data.length - 2]
+              ).toFixed(5);
+            },
           },
           {
             label: "Benediction",
             backgroundColor: "rgb(228, 87, 46)",
             borderColor: "rgb(228, 87, 46)",
             data: randomPrices(0.03, 0.05),
+            get demand() {
+              return (
+                this.data[this.data.length - 1] -
+                this.data[this.data.length - 2]
+              ).toFixed(5);
+            },
           },
           {
             label: "Earthfury",
             backgroundColor: "rgb(43, 58, 103)",
             borderColor: "rgb(43, 58, 103)",
             data: randomPrices(0.03, 0.05),
+            get demand() {
+              return (
+                this.data[this.data.length - 1] -
+                this.data[this.data.length - 2]
+              ).toFixed(5);
+            },
           },
           {
             label: "Whitemane",
             backgroundColor: "rgb(122, 199, 79)",
             borderColor: "rgb(122, 199, 79)",
             data: randomPrices(0.03, 0.05),
+            get demand() {
+              return (
+                this.data[this.data.length - 1] -
+                this.data[this.data.length - 2]
+              ).toFixed(5);
+            },
           },
         ];
         setData([...generatedData]);
